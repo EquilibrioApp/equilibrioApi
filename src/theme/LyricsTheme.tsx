e27428efb7b82cad33e331f05e4bd1b294989d8b
Estela386 <estelabarajaslozano@gmail.com>
2022-04-05 16:59:03 -0500

    Fuentes

---
+++ src/theme/LyricsTheme.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export const lyricsStyle = StyleSheet.create({
+     labelBold : {
+        top: 10,
+        marginLeft: 15,
+        fontFamily: 'Comfortaa-Bold',
+        fontSize: 20,
+        fontWeight: '300',
+        fontStyle: 'normal',
+     },
+     labelLigh: {
+        top: 10,
+        marginLeft: 10,
+        fontFamily: 'Comfortaa-Ligh',
+        fontSize: 20,
+        fontWeight: '300',
+        fontStyle: 'normal',
+     },
+     labelRegular: {
+        top: 10,
+        marginLeft: 10,
+        fontFamily: 'Comfortaa-Regular',
+        fontSize: 20,
+        fontWeight: '300',
+        fontStyle: 'normal',
+     },
+     labelInput: {
+        backgroundColor: 'value',
+        borderBottomColor: '#000000',
+        borderBottomWidth: 1,
+     }
+});
